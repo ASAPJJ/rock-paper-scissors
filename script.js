1f3f953fdd1b2d2110debbf3dc35d1d90edba102
asapjj <107642796+asapjj@users.noreply.github.com>
2026-01-21 23:36:26 +0000

    Logica eleccion de computadora
    Realice una funcion para la obtencion de un numero aleatorio para asignarle
    el valor de piedra, papel o tijeras
    
    -JJ

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World")
+
+function getComputerChoice(){
+   let jugada =  Math.floor(Math.random() * 3) + 1
+
+   if(jugada === 1){
+    return jugada = "Piedra"
+   } else if (jugada === 2){
+    return jugada = "Papel"
+   } else{
+    return jugada = "Tijeras"
+   }
+   return jugada
+} 
+
+console.log(getComputerChoice())
