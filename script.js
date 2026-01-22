6dc80f820890e34014133e27e7a88092ff88bd7d
asapjj <107642796+asapjj@users.noreply.github.com>
2026-01-22 21:47:21 +0000

    Logica del juego integrada
    Aprendi algo quizas basico, pero que la estructura Switch va unicamente en caso de opciones concretas
    no condiciones de Si/No, falta integrar todo para que se decida el ganador al mejor de 5 rondas y
    proyecto terminado
    
    -JJ

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-console.log("Hello World")
+console.log("Hello World");
 
-function getComputerChoice(){
-   let jugada =  Math.floor(Math.random() * 3) + 1
+function getComputerChoice() {
+  let jugada = Math.floor(Math.random() * 3) + 1;
 
-   if(jugada === 1){
-    return jugada = "Piedra"
-   } else if (jugada === 2){
-    return jugada = "Papel"
-   } else{
-    return jugada = "Tijeras"
-   }
-   return jugada
-} 
+  if (jugada === 1) {
+    return (jugada = "PIEDRA");
+  } else if (jugada === 2) {
+    return (jugada = "PAPEL");
+  } else {
+    return (jugada = "TIJERAS");
+  }
+}
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+  jugadaHuman = prompt("Ingrese su jugada: Piedra, Papel o Tijeras...");
+
+  return jugadaHuman.toUpperCase();
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("EMPATE");
+  } else if (
+    (humanChoice == "PIEDRA" && computerChoice == "TIJERAS") ||
+    (humanChoice == "PAPEL" && computerChoice == "PIEDRA") ||
+    (humanChoice == "TIJERAS" && computerChoice == "PAPEL")
+  ) {
+    humanScore += 1;
+    console.log(`${humanChoice} le gana a ${computerChoice}`);
+  } else {
+    computerScore += 1;
+    console.log(`${computerChoice} le gana a ${humanChoice}`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
